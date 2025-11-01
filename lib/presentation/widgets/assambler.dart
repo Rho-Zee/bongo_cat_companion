1f6662a499e70631ec04f61c32ca796feb177205
Rho-Zee <rhoxzee@gmail.com>
2025-11-01 11:24:33 +0700

    update

---
+++ lib/presentation/widgets/assambler.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Assambler extends ConsumerStatefulWidget {
+  const Assambler({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _AssamblerState();
+}
+
+class _AssamblerState extends ConsumerState<Assambler> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
